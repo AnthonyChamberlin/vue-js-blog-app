00032fe78da417092c0029dd414e3b36891a1644
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-04-15 15:12:04 +0100

    Simple Punchbag Game

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 new Vue({
     el:'#vue-app',
     data: {
-        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-        ninjas: [
-            { name: 'Ryu', age: 25 },
-            { name: 'Yoshi', age: 35 },
-            { name: 'Ken', age: 55 }
-        ]
+       health: 100,
+       ended: false
     },
     methods: {
+        punch: function() {
+            var changeStyle = document.getElementById("indicator").style;
 
+            this.health -= 10;
+
+            if (this.health <= 0) {
+                this.ended = true;
+            }
+            if (this.health <= 60) {
+                changeStyle.background = ("orange");
+            }
+            if (this.health <= 30) {
+                changeStyle.background = ("red");
+            }
+        },
+        restart: function() {
+            this.health = 100;
+            this.ended = false;
+            document.getElementById("indicator").style.background = ("green");
+        }
     },
     computed: {
 
